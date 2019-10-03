124570a2e5eac62663ac2aec170979a49d99b0cd
Seth Maisel <seth.maisel@gmail.com>
2019-10-03 21:17:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,7 +13,8 @@ end
 
 def get_japanese_emoticon(file_path = './lib/emoticons.yml', get_emoticon)
   library = load_library(file_path)
-  if library["get_emoticon"].include ? 
+  if library["get_emoticon"].include? (english_emoticon)
+    
   end
 end
 
